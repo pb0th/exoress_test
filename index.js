3eca808f026b7ccd40de52a092973f54e00b058f
pb0th <panhboth.jp@gmail.com>
2023-07-14 14:19:14 +0900

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.send("welcome");
+  return res.send("welcome to my app");
 });
 app.listen(9000, () => {
   console.log("app running on port 9000");
