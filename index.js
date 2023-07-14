ac42dc222d0c3d51e37a8604dfdb9c9ebc6b0163
pb0th <panhboth.jp@gmail.com>
2023-07-14 15:02:59 +0900

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.send("welcome mfs to my application!!!!!!!");
+  return res.send("welcome  to my application you suckers!!!!!!!");
 });
 app.listen(9000, () => {
   console.log("app running on port 9000");
